303709eda3647408e25940f556c6233bfe300c33
raphchar <50371278+raphchar@users.noreply.github.com>
2020-11-24 20:33:19 +0100

    Create Rocket.java
    Classe Rocket
    
    - manque la méthode carry -> pas très bien compris ce que le prof voulait la dessus.
    - vérifier la méthode canCarry si ça renvoie bien true ou false mais normalement c'est bon je crois

---
+++ src/Rocket.java
@@ -0,0 +1,48 @@
+public class Rocket implements SpaceShip{
+
+    private int cost;
+    private double rocketWeight;
+    private double maxWeight;
+
+    // Constructeur
+    public Rocket(int cost, double rocketWeight, double maxWeight) {
+        this.cost = cost;
+        this.rocketWeight = rocketWeight;
+        this.maxWeight = maxWeight;
+    }
+
+    //Getter
+    public int getCost() {
+        return cost;
+    }
+    public double getRocketWeight() {
+        return rocketWeight;
+    }
+    public double getMaxWeight() {
+        return maxWeight;
+    }
+
+    // Méthode de l'interface
+    @Override
+    public boolean launch() {
+        return true;
+    }
+
+    @Override
+    public boolean land() {
+        return true;
+    }
+
+    @Override
+    public boolean canCarry(Item item) {
+        return (getRocketWeight() + item.getWeight()) <= (maxWeight - rocketWeight);
+    }
+
+    @Override
+    public void carry(Item item) {
+        // A compléter pas très bien compris l'énoncé
+    }
+
+
+
+}
