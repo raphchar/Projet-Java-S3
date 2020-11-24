06d44d1642c90267f5022582402f81c26b800ec9
raphchar <50371278+raphchar@users.noreply.github.com>
2020-11-24 18:57:55 +0100

    Create Item.java
    Classe Item - Variables + Constructeur + Getter

---
+++ src/Item.java
@@ -0,0 +1,20 @@
+public class Item {
+
+    // Définition des variables
+    private final String name;
+    private final int weight;
+
+    // Constructeur
+    public Item(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    // Getter
+    public String getName() {
+        return name;
+    }
+    public int getWeight() {
+        return weight;
+    }
+}
