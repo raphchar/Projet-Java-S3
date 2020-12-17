1dd74e34a8d4c39d8c8a8a8ce42ff62fb3f1efff
raphchar <50371278+raphchar@users.noreply.github.com>
2020-12-17 11:35:25 +0100

    Garder les poids en kg + changement du random

--- src/U1.java
+++ src/U1.java
@@ -1,19 +1,20 @@
 public class U1 extends Rocket {
-    private static double random = Math.random() * 1 ;
 
     // Constructeur de U1
     public U1() {
-        super(100, 10, 18);
+        super(100, 10000, 18000);
     }
 
 
     @Override
     public boolean land() {
-        return (0.01 * (getCargoWeight()/(getMaxWeight() - getRocketWeight())) >= random);
+        double random = Math.random() * 0.1;
+        return random < (0.01 * getCargoWeight()/(getMaxWeight() - getRocketWeight()));
     }
 
     @Override
     public boolean launch() {
-        return (0.05 * (getCargoWeight()/(getMaxWeight() - getRocketWeight())) >= random);
+        double random = Math.random() * 0.1;
+        return random < (0.05 * getCargoWeight()/(getMaxWeight() - getRocketWeight()));
     }
 }
--- src/U2.java
+++ src/U2.java
@@ -1,18 +1,20 @@
 public class U2 extends Rocket {
-    double random = Math.random() * 1;
+
 
     // Constructeur de U2
     public U2() {
-        super(120,18,29);
+        super(120,18000,29000);
     }
 
     @Override
     public boolean land() {
+        double random = Math.random() * 0.8;
         return (0.08 * (getCargoWeight()/(getMaxWeight() - getRocketWeight())) >= random);
     }
 
     @Override
     public boolean launch() {
+        double random = Math.random() * 0.4;
         return (0.04 * (getCargoWeight()/(getMaxWeight() - getRocketWeight())) >= random);
     }
 }
